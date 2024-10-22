354b0286338530053e137881168225f824696530
hamidusman <abdulhamidusman218@gmail.com>
2024-10-22 12:52:40 +0100

    registration endpoint integration

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,16 +1,69 @@
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { FormEvent, useState } from "react"
+import axios from "axios"
+
+
+interface RegisterState {
+    email: string,
+    password: string,
+    confirm: string
+}
 
 const Register = () =>{
-    const iconColor = '#E68C1A'
+    const iconColor = '#E68C1A';
+    const navigate = useNavigate();
+
+    /** form data */
+    const [formState, setFormState] = useState<RegisterState>({
+        email: "",
+        password: "",
+        confirm: "",
+    })
+
+    const [error, setError] = useState<string | null>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
+    
+    /** submit function */
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+
+        if (formState.password != formState.confirm) {
+            setError("Password does not match")
+            return;
+        }
+
+        try{
+            const response = await axios.post("http://localhost:8000/auth/users/", {
+                email: formState.email,
+                password: formState.password,
+                re_password: formState.confirm
+            });
+            if(response.status === 201) {
+                navigate('/login');
+            }
+        } catch (err) {
+            setError("Failed to register");
+        }
+
+
+    };
 
     return(
         <section className="px-5 flex flex-col justify-center items-center h-[100vh]">
-            <form action="" className="w-[360px] md:w-[420px] min-h-60 bg-white shadow-2xl  flex flex-col rounded-xl pt-10 px-7">
-                <div className="font-bold text-3xl"
-                                >Register
-                </div>
+            <form 
+                onSubmit={handleSubmit}
+                className="w-[360px] md:w-[420px] min-h-60 bg-white shadow-2xl  flex flex-col rounded-xl pt-10 px-7">
+                <div className="font-bold text-3xl">Register</div>
                 <p>Fill in the form to get started</p>
                 <div className="pt-14 gap-10 text-dark flex flex-col justify-center items-center">
                     
@@ -19,7 +72,13 @@ const Register = () =>{
                         icon={faEnvelope}
                         size="xl"
                         color={iconColor}></FontAwesomeIcon>
-                        <input type="email" name="" id="" placeholder="Email" className="w-[270px] md:w-[320px] text-xl bg-transparent border-b-dark border-b-2 focus:outline-none" />
+                        <input 
+                            type="email"
+                            name="email"
+                            id="" placeholder="Email"
+                            value={formState.email}
+                            onChange={handleChange}
+                            className="w-[270px] md:w-[320px] text-xl bg-transparent border-b-dark border-b-2 focus:outline-none" />
                     </label>
                     
                     <label htmlFor="" className="flex gap-5 items-center">
@@ -27,7 +86,15 @@ const Register = () =>{
                         icon={faLock}
                         size="xl"
                         color={iconColor}></FontAwesomeIcon>
-                        <input type="password" name="" id="" placeholder="Password" className="w-[270px] md:w-[320px] text-xl bg-transparent border-b-dark border-b-2 focus:outline-none" />
+                        <input 
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            value={formState.password}
+                            onChange={handleChange}
+                            className="w-[270px] md:w-[320px] text-xl bg-transparent border-b-dark border-b-2 focus:outline-none"
+                            required
+                            />
                     </label>
                     
                     <label htmlFor="" className="flex gap-5 items-center">
@@ -35,14 +102,24 @@ const Register = () =>{
                         icon={faLock}
                         size="xl"
                         color={iconColor}></FontAwesomeIcon>
-                        <input type="password" name="" id="" placeholder="Confirm Password" className="w-[270px] md:w-[320px] text-xl bg-transparent border-b-dark border-b-2 focus:outline-none" />
+                        <input
+                            type="password"
+                            name="confirm"
+                            placeholder="Confirm Password"
+                            value={formState.confirm}
+                            onChange={handleChange}
+                            className="w-[270px] md:w-[320px] text-xl bg-transparent border-b-dark border-b-2 focus:outline-none"
+                            required
+                        />
                     </label>
+
                 </div>
                 <div className="flex flex-col justify-center gap-3 items-center my-10">
                         
                     <button type="submit" className="px-20 py-3 rounded-xl text-lg font-bold bg-primary 
-                                                  hover:bg-dark hover:text-primary
-                                                  transition duration-500 ease-in">Register</button>
+                        hover:bg-dark hover:text-primary
+                        transition duration-500 ease-in">Register</button>
+                        {error && <p className="text-red-500">{error}</p>}
                     <p>Already have an account? <Link to="/login"  className="text-primary mt-10">Login</Link> here.</p>
                 </div>
             </form>
