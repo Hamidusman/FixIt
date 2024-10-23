953cbb92cb0f32d09bd0efbb96ecc61f686af3e1
hamidusman <abdulhamidusman218@gmail.com>
2024-10-23 02:43:07 +0100

    button animation

--- src/base-components/start.tsx
+++ src/base-components/start.tsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom"
-
+import { motion } from "framer-motion"
+import { easeInOut } from "framer-motion/dom"
 const GetStarted = () =>{
     return(
-        <Link to='/booking' className="index-2 px-7 py-3 bg-primary hover:bg-dark hover:text-primary transition duration-500 text-dark font-medium rounded-2xl text-[16px] md:text-base">Start Now</Link>
+        <motion.div
+        whileHover={{rotate: '-2.5deg', scale: '1.1'}}
+        whileTap={{rotate: '2.5deg'}}
+        transition={{duration: '0.4', ease:easeInOut}}
+        >
+            <Link to='/booking' className="index-2 px-7 py-3 
+            bg-primary hover:bg-dark hover:text-primary 
+            transition duration-500 text-dark 
+            font-medium rounded-2xl text-[16px] 
+            md:text-base"
+            >
+                Start Now
+            </Link>
+        </motion.div>
     )
 }
 
